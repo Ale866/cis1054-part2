c8f77d32deaefe54100b0f8e447c3eaac0a1a729
Ale866P <alessandromancini866@gmail.com>
2024-04-20 18:59:01 +0200

    Basic setup and implemented basic navbar style

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 require_once './bootstrap.php';
 
-echo $twig->render('index.html.twig', [
-    'title' => 'Hello, World!',
-    'content' => 'This is a simple example of a Twig template.',
-]);
+echo $twig->render('index.html.twig');
