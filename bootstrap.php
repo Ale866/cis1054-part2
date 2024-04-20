4d5f5f6719dd78ff77774bfa1a8b7d6dd105b9bc
Ale866P <alessandromancini866@gmail.com>
2024-04-20 19:11:52 +0200

    Removed bootstrap.php file

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => './cache',
-]);
-
