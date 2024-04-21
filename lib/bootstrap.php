d3fe438d3fa0f36e7cc0a6c3444010d3b39cfb0b
Ale866P <alessandromancini866@gmail.com>
2024-04-21 17:42:21 +0200

    Added helper classes and basic color variables

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,6 +3,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/env.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '../cache',
-]);
+$twig = new \Twig\Environment($loader);
