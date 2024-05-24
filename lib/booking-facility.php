7b93284b779607d7a6969699b6e30858c72c7f43
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-05-25 00:17:01 +0200

    add book handling database logic

--- lib/booking-facility.php
+++ lib/booking-facility.php
@@ -1,4 +1,27 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/database/database.php';
 
-echo $twig->render('booking-facility.html.twig');
+$db = new Database();
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    $tables = $db->query("SELECT * FROM tables ORDER BY id")->fetchAll();
+
+    echo $twig->render('booking-facility.html.twig', ['tables' => $tables]);
+} elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $input = json_decode(file_get_contents('php://input'), true);
+    if (!isset($input['tables'])) {
+        http_response_code(422);
+        echo "Missing menu_id field";
+        exit;
+    }
+    $tables = $input['tables'];
+
+    foreach ($tables as $table) {
+        // add admin check 
+        $db->query('UPDATE tables SET is_taken=:is_taken WHERE id=:id', [
+            ['name' => 'is_taken', 'value' => $table['isTaken'], 'type' => SQLITE3_INTEGER],
+            ['name' => 'id', 'value' => $table['id'], 'type' => SQLITE3_INTEGER],
+        ]);
+    }
+}
