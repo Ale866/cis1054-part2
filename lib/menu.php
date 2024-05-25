bd808ab33485ed98586c59f036565edb69af1268
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-05-25 11:40:10 +0200

    add chips for menu category selection

--- lib/menu.php
+++ lib/menu.php
@@ -4,7 +4,8 @@ require_once __DIR__ . '/database/database.php';
 
 $database = new Database();
 $user_id = $_SESSION['user_id'] ?? 'NULL';
-$menu_items = $database->query("SELECT 
+$category_filter = $_GET['category'] ?? null;
+$query = "SELECT 
     m.id,
     m.name,
     m.price,
@@ -14,9 +15,19 @@ $menu_items = $database->query("SELECT
     IIF(f.id IS NOT NULL, 1,0) as favorite
     FROM menu m 
     INNER JOIN categories c ON m.category_id = c.id
-    LEFT JOIN favorites f ON f.menu_id = m.id AND f.user_id = :user_id
-", [
-    ['name' => 'user_id', 'value' => $user_id, 'type' => SQLITE3_INTEGER]
-])->fetchAll();
+    LEFT JOIN favorites f ON f.menu_id = m.id AND f.user_id = :user_id";
 
-echo $twig->render('menu.html.twig', ['menu_items' => $menu_items]);
+$params = [
+    ['name' => 'user_id', 'value' => $user_id, 'type' => SQLITE3_INTEGER]
+];
+
+if ($category_filter) {
+    $query .= " WHERE c.id = :category";
+    $params[] = ['name' => 'category', 'value' => $category_filter, 'type' => SQLITE3_INTEGER];
+}
+
+$menu_items = $database->query($query, $params)->fetchAll();
+
+$categories = $database->query("SELECT * FROM categories")->fetchAll();
+
+echo $twig->render('menu.html.twig', ['menu_items' => $menu_items, 'categories' => $categories, 'category_filter' => $category_filter ?? '']);
