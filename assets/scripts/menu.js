4c278e92311aba765ac733481301f2be577bbfb4
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-05-27 17:38:19 +0200

    change menu filters structure from chips to select

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -50,3 +50,12 @@ function undoFavoriteIcon(elem) {
     elem.classList.add('fa-regular')
     elem.classList.remove('fa-solid')
 }
+
+const categorySelect = document.getElementById('category-select')
+categorySelect.addEventListener('change', function () {
+    if (this.value == 'All') {
+        window.location.href = window.location.pathname
+    } else {
+        window.location.href = window.location.pathname + `?category=${this.value}`
+    }
+});
